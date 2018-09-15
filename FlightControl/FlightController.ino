336e8346b163fe0359888461e96028fdfa589da2
Arnór <Arnr@Bortlva>
2018-09-15 15:28:57 +0000

    update

---
+++ FlightControl/FlightController.ino
@@ -0,0 +1,15 @@
+﻿/*
+ Name:		Sketch2.ino
+ Created:	09/15/2018 14:31:56
+ Author:	Arnór
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
